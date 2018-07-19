47c7c1a75626978db7449218bbccc750969815d5
Unknown <bruno.zeroleft@gmail.com>
2018-07-19 18:08:48 -0400

    actualizar ciudad
    numero requerimiento: 4
    modificaciones:
    - implementacion metodo de actualizacion de descripcion de la entidad Ciudad.

--- src/main/java/cl/devetel/test/ubicacionservice/controller/CiudadRestController.java
+++ src/main/java/cl/devetel/test/ubicacionservice/controller/CiudadRestController.java
@@ -57,5 +57,27 @@ public class CiudadRestController {
 		ciudadRepository.delete(ciudad);
 		return new ResponseEntity<String>(mensaje, HttpStatus.OK);
 	}
+	
+	/**
+	 * Actualiza una ciudad
+	 * @param id
+	 * @return
+	 * @author bverad
+	 */
+	@RequestMapping(value="ciudad/actualizar/{id}/descripcion/{descripcion}", method=RequestMethod.GET)
+	public ResponseEntity<String> elinarCiudadPorId(@PathVariable(name="id") Integer id,
+			@PathVariable(name="descripcion") String descripcion){
+		Ciudad ciudad = ciudadRepository.findById(id).get();
+		String mensaje = "Error";
+		if(ciudad == null) {
+			mensaje = "No existe la ciudad que desea actualizar.";
+		}else {
+			ciudad.setDescripcion(descripcion);
+			ciudadRepository.save(ciudad);
+			mensaje = "Ciudad actualizada con exito : " + ciudad.getDescripcion();
+		}
+		
+		return new ResponseEntity<String>(mensaje, HttpStatus.OK);
+	}
 
 }
